7f60c95a91dcf526ab7433b8c9174d731121e0f7
simadivdar <simadivdar@gmail.com>
2023-11-01 15:41:49 +0330

    feat : add apicall  weather page

--- src/pages/Weather.vue
+++ src/pages/Weather.vue
@@ -1,7 +1,71 @@
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+interface WeatherData {
+  current_temperature: number;
+  current_weather: string;
+  weather_units:string;
+}
 export default defineComponent({
   name: "Weather",
+  setup() {
+    const weatherData = ref<WeatherData | null>(null);
+    const loading = ref(true);
+    const error = ref(false);
+    const getInformation = ref(false);
+    const city_name = ref("");
+    const errorMessage = ref("");
+    const latitude = ref();
+    const longitude = ref();
+    const getCity = async () => {
+      try {
+        error.value=false;
+        getInformation.value=true;
+       
+        const response = await fetch(
+          `http://localhost:3000/cities?city=${city_name.value}`
+        );
+        const data = await response.json();
+        console.log(data);
+        latitude.value = data[0].lat;
+        longitude.value = data[0].lng;
+        getWeather();
+        loading.value = false;
+      } catch (e) {
+        errorMessage.value="Not Found City..."
+        error.value = true;
+        loading.value = false;
+      }
+    };
+
+    const getWeather = async () => {
+      try {
+        const response = await fetch(
+          `https://api.open-meteo.com/v1/forecast?latitude=${latitude.value}&longitude=${longitude.value}&current_weather=true`
+        );
+        const data = await response.json();
+        weatherData.value = {
+          current_temperature: data.current_weather.temperature,
+          current_weather: data.current_weather.weathercode,
+          weather_units:data.current_weather_units.temperature
+        };
+        loading.value = false;
+      } catch (e) {
+        errorMessage.value="Try again.."
+        error.value = true;
+        loading.value = false;
+      }
+    };
+
+    return {
+      weatherData,
+      loading,
+      city_name,
+      error,
+      errorMessage,
+      getInformation,
+      getCity,
+    };
+  },
 });
 </script>
 
@@ -10,16 +74,21 @@ export default defineComponent({
     <input
       class="form-control"
       type="text"
+      v-model="city_name"
+      @keyup.enter="getCity"
       placeholder="Enter city .."
       aria-label="default input name"
     />
     <div class="d-flex justify-content-center">
+      <div v-if="loading && getInformation">loading...</div>
+      <div v-else-if="error">{{  errorMessage }}</div>
       <div
+        v-else
         class="mt-5 h-25 w-50 border d-flex flex-column justify-content-center"
       >
-        <p>Tehran</p>
-        <p>16</p>
-        <p>winy</p>
+        <p>{{ city_name }}</p>
+        <p>{{ weatherData?.current_temperature }}{{ weatherData?.weather_units}}</p>
+        <p>{{ weatherData?.current_weather }}</p>
       </div>
     </div>
   </div>
