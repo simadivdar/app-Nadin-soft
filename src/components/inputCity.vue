acf5a870a6b4f5304f9f5f5957ae3dc4fd35e7de
simadivdar <simadivdar@gmail.com>
2023-12-10 12:38:52 +0330

    fix :change state to provide/inject

---
+++ src/components/inputCity.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import showWeather from "./showWeather.vue";
+const city_name = ref<string>("");
+const show = ref<boolean>(false);
+const name = computed((): string => {
+  return city_name.value.charAt(0).toUpperCase() + city_name.value.slice(1);
+});
+</script>
+
+<template>
+  <div class="mt-5">
+    <div class="col-4">
+      <input
+        class="form-control text-center"
+        type="text"
+        v-model.lazy.trim="city_name"
+        @keyup.enter="show = !show"
+        :placeholder="$t('Enter city')"
+        aria-label="default input name"
+      />
+    </div>
+    <showWeather v-if="show" :name="name" />
+  </div>
+</template>
+
+<style scoped></style>
